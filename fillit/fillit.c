995c7ab108ca9d4f03251e75060b1337d82149ea
Gerald Wyman manderly <gwyman-m@e2r6p3.21-school.ru>
2019-03-08 18:40:15 +0300

    upd

--- fillit/fillit.c
+++ fillit/fillit.c
@@ -3,6 +3,29 @@
 #include <fcntl.h>
 #include <stdio.h>
 
+int		check_valid(int fd)
+{
+	char	line[21];
+	int		r;
+	int		i;
+	int		k;
+
+	i = 0;
+	k = 0;
+	while (k < 26)
+	{
+		while ((i < 5) && ((r = get_next_line(fd, &line)) > 0))
+		{
+
+		}
+		//konec
+		i = 0;
+		k++;
+	}
+}
+
+
+
 int		get_figures(int fd)
 {
 	char	line_sep[5][5];
@@ -11,7 +34,6 @@ int		get_figures(int fd)
 
 	while ((r = read(fd, line, len)) > 0)
 	{
-
 	}
 }
 
@@ -26,6 +48,8 @@ int		main(int argc, char **argv)
 	if (argc  == 2)
 	{
 		fd = open(argv[1], O_RDONLY);
+		if (check_valid(fd) == -1)
+			return (-1); //PRINTERROR VALID
 		get_figures(fd);
 	}
 	return (1);
